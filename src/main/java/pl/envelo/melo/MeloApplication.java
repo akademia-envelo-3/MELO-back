76b960d17786d0f65c1c2db9990d14bb3580506e
Albert Torzewski <albert.torzewski@ppuc.pl>
2023-01-02 15:09:35 +0100

    init: Added project structure

---
+++ src/main/java/pl/envelo/melo/MeloApplication.java
@@ -0,0 +1,13 @@
+package pl.envelo.melo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/envelo/melo/MeloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.envelo.melo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
