b4b25344b7ee64c37fcf88459dc37e6fc2752019
Albert Torzewski <albert.torzewski@ppuc.pl>
2023-01-05 15:04:35 +0100

    feat INT-09: Implemented notification context classes
    Added controller, service, repository and dtos for notification context

---
+++ src/main/java/pl/envelo/melo/domain/notification/NotificationService.java
@@ -0,0 +1,40 @@
+package pl.envelo.melo.domain.notification;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import pl.envelo.melo.domain.notification.dto.EventNotificationDto;
+import pl.envelo.melo.domain.notification.dto.NotificationDto;
+import pl.envelo.melo.domain.notification.dto.RequestNotificationDto;
+import pl.envelo.melo.domain.notification.dto.UnitNotificationDto;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class NotificationService {
+    public NotificationRepository notificationRepository;
+
+    public ResponseEntity<EventNotificationDto> insertEventNotification(EventNotificationDto eventNotificationDto) {
+        return null;
+    }
+
+    public ResponseEntity<RequestNotificationDto> insertRequestNotification(RequestNotificationDto requestNotificationDto) {
+        return null;
+    }
+
+    public ResponseEntity<UnitNotificationDto> insertUnitNotification(UnitNotificationDto unitNotificationDto) {
+        return null;
+    }
+
+    public ResponseEntity<List<NotificationDto>> listAllNotification(int employeeId) {
+        return null;
+    }
+
+    public ResponseEntity<List<NotificationDto>> listUncheckedNotification(int employeeId) {
+        return null;
+    }
+
+    public ResponseEntity<?> setNotificationAsChecked(int notificationId) {
+        return null;
+    }
+}
