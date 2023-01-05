6779b05d39669eec873d83966303deeeba976577
Jakub Madej <jakub.madej@ppuc.pl>
2023-01-05 15:36:37 +0100

    feat INT-07: added class template for Attachment context

---
+++ src/main/java/pl/envelo/melo/domain/attachment/dto/AttachmentDto.java
@@ -0,0 +1,24 @@
+package pl.envelo.melo.domain.attachment.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+import pl.envelo.melo.domain.attachment.AttachmentType;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttachmentDto {
+
+    @NotBlank(message = "name of attachment can't be blank")
+    @Max(255)
+    private String name;
+
+    @URL(message = "Wrong format of attachment URL")
+    private String attachmentUrl;
+
+    @NotBlank(message = "temporary validation. attachmentType can't be null")
+    private AttachmentType attachmentType;
+}
