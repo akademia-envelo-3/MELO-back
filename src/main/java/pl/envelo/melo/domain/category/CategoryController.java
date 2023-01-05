cf86563ad8cd94b39b9dedc62165429b77a98dc2
barbara.kowalewska <bbkowalewska@gmail.com>
2023-01-05 14:11:59 +0100

    feat INT-08 Added controller, service, dto, repository for Category.java

---
+++ src/main/java/pl/envelo/melo/domain/category/CategoryController.java
@@ -0,0 +1,34 @@
+package pl.envelo.melo.domain.category;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    CategoryDto addNewCategory(CategoryDto categoryDto) {
+        return null;
+    }
+
+    CategoryDto getCategory(int id) {
+        return null;
+    }
+
+    CategoryDto hideCategory(int id) {
+        return null;
+    }
+
+    CategoryDto activateCategory(int id) {
+        return null;
+    }
+
+    List<Category> showAllCategory() {
+        return null;
+    }
+}
---
+++ src/main/java/pl/envelo/melo/domain/category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package pl.envelo.melo.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
