aa16371c5e5c5931f344793e86326039869c3e39
Basia413 <49940687+Basia413@users.noreply.github.com>
2023-01-09 16:14:17 +0100

    Revert "Int 06"

--- src/main/java/pl/envelo/melo/domain/comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.envelo.melo.domain.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
