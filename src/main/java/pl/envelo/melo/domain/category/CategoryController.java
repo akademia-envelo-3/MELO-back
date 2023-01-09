4af6b91e1ab90aed6d9d1c135e6ffdc23c16a245
barbara.kowalewska <bbkowalewska@gmail.com>
2023-01-09 11:36:19 +0100

    feat INT-11 Corrections

--- src/main/java/pl/envelo/melo/domain/category/CategoryController.java
+++ src/main/java/pl/envelo/melo/domain/category/CategoryController.java
@@ -1,17 +1,15 @@
 package pl.envelo.melo.domain.category;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
-
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
 
     public ResponseEntity<CategoryDto> addNewCategory(CategoryDto categoryDto) {
         return null;
