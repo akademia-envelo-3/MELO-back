01e7afe545124cbbef33078c8e41bc47bfcba279
Jakub Madej <jakub.madej@ppuc.pl>
2023-01-09 12:23:23 +0100

    feat INT-15: added class template for Location context

---
+++ src/main/java/pl/envelo/melo/domain/location/LocationService.java
@@ -0,0 +1,22 @@
+package pl.envelo.melo.domain.location;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.envelo.melo.domain.location.dto.LocationDto;
+
+@RequiredArgsConstructor
+@Service
+public class LocationService {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    public Location insertNewLocation(LocationDto locationDto) {
+        return null;
+    }
+
+    public LocationDto getLocation(long id) {
+        return null;
+    }
+}
