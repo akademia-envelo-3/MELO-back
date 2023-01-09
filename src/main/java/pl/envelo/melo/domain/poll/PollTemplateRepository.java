5aeb6826a7a20d9c169f67aa6dd6f1fb1a186562
Albert Torzewski <albert.torzewski@ppuc.pl>
2023-01-09 13:00:57 +0100

    feat INT-14: Implemented poll context classes
    Added controller, service, repository and dtos for poll context

---
+++ src/main/java/pl/envelo/melo/domain/poll/PollTemplateRepository.java
@@ -0,0 +1,6 @@
+package pl.envelo.melo.domain.poll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PollTemplateRepository extends JpaRepository<PollTemplate, Integer> {
+}
