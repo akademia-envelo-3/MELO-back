02ebe8ae0dfaf8d2b8627fb9a4ac055ce5961244
Jakub Madej <jakub.madej@ppuc.pl>
2023-01-09 12:23:23 +0100

    feat INT-15: added class template for Location context

---
+++ src/main/java/pl/envelo/melo/domain/location/LocationRepository.java
@@ -0,0 +1,6 @@
+package pl.envelo.melo.domain.location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+}
