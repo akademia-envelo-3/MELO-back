064a27b4076548582f2c94505d28f34bc8978273
iLoveBingo <kuba.madej@hotmail.com>
2023-01-12 14:10:55 +0100

    feat: added insertNewComment method body

--- src/main/java/pl/envelo/melo/domain/comment/CommentService.java
+++ src/main/java/pl/envelo/melo/domain/comment/CommentService.java
@@ -5,19 +5,28 @@ import org.springframework.stereotype.Service;
 import pl.envelo.melo.domain.attachment.Attachment;
 import pl.envelo.melo.domain.attachment.AttachmentRepository;
 import pl.envelo.melo.domain.comment.dto.CommentDto;
+import pl.envelo.melo.mappers.CommentMapper;
+
+import java.net.URI;
+import java.util.Optional;
+
 
 @Service
 public class CommentService {
 
     private CommentRepository commentRepository;
     private AttachmentRepository attachmentRepository;
+    private CommentMapper commentMapper;
 
-    public CommentService(CommentRepository commentRepository, AttachmentRepository attachmentRepository){
+    public CommentService(CommentRepository commentRepository, AttachmentRepository attachmentRepository, CommentMapper commentMapper){
         this.commentRepository = commentRepository;
         this.attachmentRepository = attachmentRepository;
+        this.commentMapper = commentMapper;
     }
 
     public ResponseEntity<Comment> insertNewComment(CommentDto commentToSave){
-        return null;
+        Comment mappedComment = commentMapper.convert(commentToSave);
+        Comment createdCommentFromDb = commentRepository.save(mappedComment);
+        return ResponseEntity.ok(createdCommentFromDb);
     }
 }
