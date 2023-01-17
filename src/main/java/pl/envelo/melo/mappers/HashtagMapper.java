1fc26c4500af1a3ec6580ac99452feca055aceb3
Basia413 <49940687+Basia413@users.noreply.github.com>
2023-01-17 11:58:29 +0100

    Eve 09 (#96)
    * feat EVE-09 Added method incrementHashtagGlobalCount and test for it
    
    * feat EVE-09 Added method insertNewHashtag and test for it and mapper
    
    Co-authored-by: barbara.kowalewska <bbkowalewska@gmail.com>

--- src/main/java/pl/envelo/melo/mappers/HashtagMapper.java
+++ src/main/java/pl/envelo/melo/mappers/HashtagMapper.java
@@ -11,7 +11,12 @@ import pl.envelo.melo.domain.hashtag.HashtagDto;
 @Mapper(componentModel = "spring")
 public interface HashtagMapper {
 
-    default String  convert(Hashtag hashtag){
+
+    Hashtag convert(HashtagDto hashtagDto);
+    @InheritInverseConfiguration
+    HashtagDto convert(Hashtag hashtag);
+    
+    default String  convertToString(Hashtag hashtag){
         return hashtag.getContent();
     }
 }
---
+++ src/test/java/pl/envelo/melo/domain/hashtag/HashtagServiceTest.java
@@ -0,0 +1,60 @@
+package pl.envelo.melo.domain.hashtag;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import pl.envelo.melo.mappers.HashtagMapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HashtagServiceTest {
+
+    @Autowired
+    HashtagRepository hashtagRepository;
+    @Autowired
+    HashtagService hashtagService;
+    @Autowired
+    HashtagMapper hashtagMapper;
+    @Test
+    @Transactional
+    void incrementHashtagGlobalCount() {
+        Hashtag hashtag = new Hashtag();
+        hashtag.setId(1);
+        hashtag.setContent("Coffe");
+        hashtag.setGlobalUsageCount(1);
+        hashtagRepository.save(hashtag);
+
+        ResponseEntity<?> testhashtag = hashtagService.incrementHashtagGlobalCount(1);
+        //Correct request
+        assertEquals(HttpStatus.OK, testhashtag.getStatusCode());
+        Hashtag hashtag1 = (Hashtag) testhashtag.getBody();
+        //Right value
+        assertEquals(2,hashtag1.getGlobalUsageCount());
+        //Incorrect request
+        assertEquals(HttpStatus.valueOf(404), hashtagService.incrementHashtagGlobalCount(200).getStatusCode());
+    }
+
+    @Test
+    @Transactional
+    void insertNewHashtag() {
+        Hashtag hashtag = new Hashtag();
+        hashtag.setId(1);
+        hashtag.setContent("Coffe");
+        hashtag.setGlobalUsageCount(1);
+        hashtagRepository.save(hashtag);
+        ResponseEntity<HashtagDto> responseHashtag = hashtagService.insertNewHashtag(hashtagMapper.convert(hashtag));
+        assertEquals(HttpStatus.OK, responseHashtag.getStatusCode());
+        Hashtag hashtag1 = hashtagMapper.convert(responseHashtag.getBody());
+        assertEquals(2,hashtagRepository.findByContent(responseHashtag.getBody().getContent()).getGlobalUsageCount());
+        HashtagDto hashtagDto = new HashtagDto();
+        hashtagDto.setContent("Game");
+        responseHashtag = hashtagService.insertNewHashtag(hashtagDto);
+        assertEquals(HttpStatus.OK, responseHashtag.getStatusCode());
+        assertTrue(hashtagRepository.existsByContent("Game"));
+
+    }
+}
