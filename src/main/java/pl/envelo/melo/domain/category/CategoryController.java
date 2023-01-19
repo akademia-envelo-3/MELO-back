fdf7b98293e14c295358ee1d571a2c1b37ffe98d
Przemyslaw M <p.mroczek@onet.pl>
2023-01-19 14:38:33 +0100

    feat EVE-18 added getCategory, getListOfCategories and addNewCategory methods

--- src/main/java/pl/envelo/melo/domain/category/CategoryController.java
+++ src/main/java/pl/envelo/melo/domain/category/CategoryController.java
@@ -2,32 +2,38 @@ package pl.envelo.melo.domain.category;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("category")
 public class CategoryController {
     private final CategoryService categoryService;
 
+
+    @PostMapping("")
     public ResponseEntity<CategoryDto> addNewCategory(CategoryDto categoryDto) {
-        return null;
+        return categoryService.insertNewCategory(categoryDto);
     }
 
-    public ResponseEntity<Category> getCategory(int id) {
-        return null;
+    @GetMapping("{id}")
+    public ResponseEntity<?> getCategory(@RequestParam("id") int id) {
+        return categoryService.getCategory(id);
     }
+
 
     public ResponseEntity<?> hideCategory(int id) {
-        return null;
+        return categoryService.changeStatusCategory(id);
     }
 
     public ResponseEntity<?> activateCategory(int id) {
-        return null;
+        return categoryService.changeStatusCategory(id);
     }
 
-    public ResponseEntity<List<Category>> showAllCategory() {
-        return null;
+    @GetMapping("")
+    public ResponseEntity<List<CategoryDto>> showAllCategory() {
+        return categoryService.listAllCategory();
     }
 }
--- src/main/java/pl/envelo/melo/domain/category/CategoryService.java
+++ src/main/java/pl/envelo/melo/domain/category/CategoryService.java
@@ -1,8 +1,11 @@
 package pl.envelo.melo.domain.category;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pl.envelo.melo.domain.event.Event;
+import pl.envelo.melo.mappers.CategoryMapper;
 
 import java.util.List;
 
@@ -10,21 +13,30 @@ import java.util.List;
 @AllArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
 
-    public ResponseEntity<CategoryDto> changeStatusCategory(int id) {
+    public ResponseEntity<Boolean> changeStatusCategory(int id) {
+
         return null;
     }
 
     public ResponseEntity<CategoryDto> insertNewCategory(CategoryDto categoryDto) {
-        return null;
+        Category category = categoryMapper.toEntity(categoryDto);
+        category.setHidden(false);
+        return new ResponseEntity(categoryRepository.save(category), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<CategoryDto> getCategory(int id) {
-        return null;
+    public ResponseEntity<?> getCategory(int id) {
+        if(categoryRepository.existsById(id)) {
+            Category category = categoryRepository.findById(id).get();
+            return ResponseEntity.ok(categoryMapper.toDto(category));
+        }
+        else return ResponseEntity.status(404).body("Category with given ID does not exist in database");
     }
 
     public ResponseEntity<List<CategoryDto>> listAllCategory() {
-        return null;
+        List<Category> listOfCategories = categoryRepository.findAll();
+        return ResponseEntity.ok(listOfCategories.stream().map(categoryMapper::toDto).toList());
     }
 }
