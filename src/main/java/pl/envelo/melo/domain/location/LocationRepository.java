34df4232028167119848f9013dbbea504463d443
MarekGFx <marekgajdamowicz6@wp.pl>
2023-01-19 15:54:19 +0100

    feat EVE-17:body method insertOrGetLocation added

--- src/main/java/pl/envelo/melo/domain/location/LocationRepository.java
+++ src/main/java/pl/envelo/melo/domain/location/LocationRepository.java
@@ -2,14 +2,13 @@ package pl.envelo.melo.domain.location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
 
     Optional<Location> findByStreetName(String streetName);
-//    Optional<Location> findByStreetNameByStreetNumberByApartmentNumberByPostalCodeByCity(
-//            String streetName,String streetNumber,String apartmentNumber, String postalCode, String city
-//    );
+    List<Location> findListByStreetName(String streetName);
 
 }
--- src/main/java/pl/envelo/melo/domain/location/LocationService.java
+++ src/main/java/pl/envelo/melo/domain/location/LocationService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.envelo.melo.domain.location.dto.LocationDto;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class LocationService {
@@ -15,23 +17,36 @@ public class LocationService {
     public Location insertNewLocation(LocationDto locationDto) {
         return null;
     }
+
     public Location insertOrGetLocation(LocationDto locationDto) {
+
         Location newLocation = new Location();
         newLocation.setStreetName(locationDto.getStreetName());
         newLocation.setStreetName(locationDto.getStreetNumber());
         newLocation.setApartmentNumber(locationDto.getApartmentNumber());
         newLocation.setPostalCode(locationDto.getPostalCode());
         newLocation.setCity(locationDto.getCity());
-        if (locationRepository.findByStreetName(newLocation.getStreetName()).isPresent()){
 
+        if (locationRepository.findByStreetName(newLocation.getStreetName()).isPresent()) {
 
+            List<Location> toFindLocations = locationRepository.findListByStreetName(newLocation.getStreetName()).stream().toList();
+            for (Location location : toFindLocations) {
+                if (location.getStreetName().equals(newLocation.getStreetName())
+                        && location.getStreetNumber().equals(newLocation.getStreetNumber())
+                        && location.getApartmentNumber().equals(newLocation.getApartmentNumber())
+                        && location.getPostalCode().equals(newLocation.getPostalCode())
+                        && location.getCity().equals(newLocation.getCity())) {
 
+                    return location;
+                }
+            }
         } else {
-            locationRepository.save(newLocation);
+        locationRepository.save(newLocation);
         }
         return newLocation;
 
-    }
+}
+
     public LocationDto getLocation(long id) {
         return null;
     }
