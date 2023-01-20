c5c2e0b72e06594c68f6a25b7d0c377c2c5fbc71
iLoveBingo <kuba.madej@hotmail.com>
2023-01-20 11:15:02 +0100

    First implementation of basic classes to service Upload funcionality

---
+++ src/main/java/pl/envelo/melo/domain/attachment/uploadmultiple/ResponseMessage.java
@@ -0,0 +1,18 @@
+package pl.envelo.melo.domain.attachment.uploadmultiple;
+
+public class ResponseMessage {
+    private String message;
+
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
