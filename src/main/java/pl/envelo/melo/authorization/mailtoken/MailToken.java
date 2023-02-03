345de10f9e74e16953fadd8d522cb3a2be6c0b6a
barbara.kowalewska <bbkowalewska@gmail.com>
2023-02-03 11:03:00 +0100

    Feat EVE-12 Added method generateToken, refactor entity MailToken

--- src/main/java/pl/envelo/melo/authorization/mailtoken/MailToken.java
+++ src/main/java/pl/envelo/melo/authorization/mailtoken/MailToken.java
@@ -6,19 +6,22 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.envelo.melo.authorization.person.Person;
 import pl.envelo.melo.domain.event.Event;
+
+import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MailToken {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    int id;
-    String token;
+    UUID token;
     Person person;
     Event event;
 }
--- src/main/java/pl/envelo/melo/authorization/mailtoken/MailTokenService.java
+++ src/main/java/pl/envelo/melo/authorization/mailtoken/MailTokenService.java
@@ -2,6 +2,7 @@ package pl.envelo.melo.authorization.mailtoken;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.envelo.melo.authorization.person.PersonRepository;
 import pl.envelo.melo.domain.event.EventRepository;
 
 @Service
@@ -9,8 +10,13 @@ import pl.envelo.melo.domain.event.EventRepository;
 public class MailTokenService {
     private final MailTokenRepository mailTokenRepository;
     private final EventRepository eventRepository;
+    private final PersonRepository personRepository;
 
-    public MailToken genereteToken(int eventId, int personId){
-        return null;
+    public MailToken generateToken(int eventId, int personId){
+        MailToken token = new MailToken();
+        token.setPerson(personRepository.findById(personId).get());
+        token.setEvent(eventRepository.findById(eventId).get());
+        mailTokenRepository.save(token);
+        return token;
     }
 }
