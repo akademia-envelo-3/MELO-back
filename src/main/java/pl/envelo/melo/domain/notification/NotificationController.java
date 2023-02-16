53f84d50c9a4318747405ebc079adef12c3d1ba1
Przemyslaw M <p.mroczek@onet.pl>
2023-02-16 14:10:39 +0100

    feat BOX-01 added retrieving list of notifications for employee, created NotificationMapper

--- src/main/java/pl/envelo/melo/domain/notification/NotificationController.java
+++ src/main/java/pl/envelo/melo/domain/notification/NotificationController.java
@@ -1,18 +1,38 @@
 package pl.envelo.melo.domain.notification;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.envelo.melo.domain.notification.dto.NotificationDto;
+import pl.envelo.melo.domain.unit.dto.UnitToDisplayOnListDto;
 
+import java.security.Principal;
 import java.util.List;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping("v1/notifications")
 public class NotificationController {
     private NotificationService notificationService;
 
-    public ResponseEntity<NotificationDto> showAllNotifications(int employeeId) {
-        return null;
+    @PreAuthorize("hasAuthority(@securityConfiguration.getEmployeeRole())")
+    @GetMapping("")
+    @Operation(summary = "Retrieve list of notifications",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Retrieve list of notifications for user that is executing this method", content =
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = NotificationDto.class)))
+            })
+    public ResponseEntity<List<NotificationDto>> showAllNotifications(Principal principal) {
+        return notificationService.listAllNotification(principal);
     }
 
     public ResponseEntity<List<NotificationDto>> showNewNotifications(int employeeId) {
