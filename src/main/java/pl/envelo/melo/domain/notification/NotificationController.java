b6e45bb0bae7b33481b7e2d5938ac6c9910513f3
przemyslawmroczek <49682737+przemyslawmroczek@users.noreply.github.com>
2023-02-20 10:28:07 +0100

    feat BOX-01 added retrieving list of notifications for employee, crea… (#193)
    * feat BOX-01 added retrieving list of notifications for employee, created NotificationMapper
    
    * feat BOX-01 updated swagger documentation

--- src/main/java/pl/envelo/melo/domain/notification/NotificationController.java
+++ src/main/java/pl/envelo/melo/domain/notification/NotificationController.java
@@ -1,18 +1,40 @@
 package pl.envelo.melo.domain.notification;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.envelo.melo.domain.notification.dto.NotificationDto;
+import pl.envelo.melo.domain.unit.dto.UnitToDisplayOnListDto;
 
+import java.security.Principal;
 import java.util.List;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping("v1/notifications")
 public class NotificationController {
     private NotificationService notificationService;
 
-    public ResponseEntity<NotificationDto> showAllNotifications(int employeeId) {
-        return null;
+    @PreAuthorize("hasAuthority(@securityConfiguration.getEmployeeRole())")
+    @GetMapping("")
+    @Operation(summary = "Retrieve list of notifications",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Retrieve list of notifications for user that is executing this method", content =
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = NotificationDto.class))),
+                    @ApiResponse(responseCode = "401", description = "User executing this method is not an Employee"),
+                    @ApiResponse(responseCode = "403", description = "Employee connected with this token is not present in database")
+            })
+    public ResponseEntity<List<NotificationDto>> showAllNotifications(Principal principal) {
+        return notificationService.listAllNotification(principal);
     }
 
     public ResponseEntity<List<NotificationDto>> showNewNotifications(int employeeId) {
--- src/main/java/pl/envelo/melo/domain/notification/NotificationService.java
+++ src/main/java/pl/envelo/melo/domain/notification/NotificationService.java
@@ -1,20 +1,31 @@
 package pl.envelo.melo.domain.notification;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pl.envelo.melo.authorization.AuthorizationService;
+import pl.envelo.melo.authorization.admin.Admin;
+import pl.envelo.melo.authorization.employee.Employee;
+import pl.envelo.melo.authorization.employee.EmployeeRepository;
 import pl.envelo.melo.domain.notification.dto.EventNotificationDto;
 import pl.envelo.melo.domain.notification.dto.NotificationDto;
 import pl.envelo.melo.domain.notification.dto.RequestNotificationDto;
 import pl.envelo.melo.domain.notification.dto.UnitNotificationDto;
+import pl.envelo.melo.mappers.NotificationMapper;
 
-import java.util.List;
-import java.util.Optional;
+import java.security.Principal;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class NotificationService {
     private NotificationRepository notificationRepository;
+
+    @Autowired
+    private final AuthorizationService authorizationService;
+    private final EmployeeRepository employeeRepository;
+    private final NotificationMapper notificationMapper;
 
     public ResponseEntity<EventNotificationDto> insertEventNotification(EventNotificationDto eventNotificationDto) {
         return null;
@@ -28,8 +39,22 @@ public class NotificationService {
         return null;
     }
 
-    public ResponseEntity<List<NotificationDto>> listAllNotification(int employeeId) {
-        return null;
+    public ResponseEntity<List<NotificationDto>> listAllNotification(Principal principal) {
+        authorizationService.inflateUser(principal);
+        Employee employee = employeeRepository.findByUserId(authorizationService.getUUID(principal)).orElse(null);
+        if(Objects.nonNull(employee)) {
+            if(Objects.isNull(employee.getNotificationsBox())) {
+                return ResponseEntity.ok(new ArrayList<>());
+            }
+            List<Notification> checkedNotifications = new ArrayList<>(employee.getNotificationsBox().stream().filter(Notification::isChecked).toList());
+            List<Notification> notifications = new ArrayList<>(employee.getNotificationsBox().stream().filter(notification -> !notification.isChecked()).toList());
+            checkedNotifications.sort(Comparator.comparing(Notification::getTimestamp).reversed());
+            notifications.sort(Comparator.comparing(Notification::getTimestamp).reversed());
+            notifications.addAll(checkedNotifications);
+            return ResponseEntity.ok(notificationMapper.toDto(notifications));
+        } else {
+            return ResponseEntity.status(403).build();
+        }
     }
 
     public ResponseEntity<List<NotificationDto>> listUncheckedNotification(int employeeId) {
