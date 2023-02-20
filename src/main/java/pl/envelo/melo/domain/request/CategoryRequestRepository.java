fb25a90dedf7505e3b8316a51ea23bc38cee8f86
Albi7331 <38432511+Albi7331@users.noreply.github.com>
2023-02-20 11:50:28 +0100

    feat BOX-02: Implemented admin inbox functionality (#191)
    * Implemented admin inbox functionality
    
    Removed Admin to CategoryRequests relation
    
    Added getters to retrieve employee firstname and lastname
    
    * Adjusted the code to reuse existing mapper

--- src/main/java/pl/envelo/melo/domain/request/CategoryRequestRepository.java
+++ src/main/java/pl/envelo/melo/domain/request/CategoryRequestRepository.java
@@ -2,5 +2,8 @@ package pl.envelo.melo.domain.request;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRequestRepository extends JpaRepository<CategoryRequest, Integer> {
+    List<CategoryRequest> findAllByIsResolved(boolean resolved);
 }
