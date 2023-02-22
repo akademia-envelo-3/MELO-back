0d9b617e2ff46f40ff919d741bd2b7ce06c221ab
barbara.kowalewska <bbkowalewska@gmail.com>
2023-02-22 13:52:19 +0100

    feat INT-21 Optimize imports after merge

--- src/main/java/pl/envelo/melo/exceptions/ApplicationExceptionHandler.java
+++ src/main/java/pl/envelo/melo/exceptions/ApplicationExceptionHandler.java
@@ -3,7 +3,6 @@ package pl.envelo.melo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
