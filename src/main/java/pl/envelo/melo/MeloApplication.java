0190bdd3b66dead462494163b3eab16716c0dcb7
Albi7331 <albi@onet.com.pl>
2023-02-28 15:36:52 +0100

    Removed unnecessary imports

--- src/main/java/pl/envelo/melo/MeloApplication.java
+++ src/main/java/pl/envelo/melo/MeloApplication.java
@@ -2,9 +2,6 @@ package pl.envelo.melo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
 @SpringBootApplication
 public class MeloApplication {
     public static void main(String[] args) {
