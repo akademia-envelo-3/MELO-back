721e5aaad3816cf37fb5379cefafb350ac2bdd4b
MarekGFx <marekgajdamowicz6@wp.pl>
2023-03-01 16:14:54 +0100

    DOC-UNIT: added documentation for location package

--- src/main/java/pl/envelo/melo/domain/location/LocationService.java
+++ src/main/java/pl/envelo/melo/domain/location/LocationService.java
@@ -9,6 +9,10 @@ import pl.envelo.melo.exceptions.LocationBadRequestException;
 
 import java.util.List;
 
+/**
+ The LocationService class is responsible for handling operations related to Location objects.
+ It contains a method for inserting or getting a Location object based on the data provided in a LocationDto object.
+ */
 @RequiredArgsConstructor
 @Service
 @Transactional
@@ -17,6 +21,16 @@ public class LocationService {
     @Autowired
     LocationRepository locationRepository;
 
+    /**
+     Inserts or retrieves a Location object based on the data provided in the LocationDto object.
+     If all fields in the LocationDto object are empty, null is returned.
+     If any of the required fields in the LocationDto object are empty, a LocationBadRequestException is thrown.
+     If a Location object with the same data already exists in the database, that object is returned.
+     Otherwise, a new Location object is created, saved to the database, and returned.
+     @param locationDto The LocationDto object containing the data for the Location object.
+     @return The Location object created or retrieved based on the data in the LocationDto object.
+     @throws LocationBadRequestException If any of the required fields in the LocationDto object are empty.
+     */
     public Location insertOrGetLocation(LocationDto locationDto) {
 
         if (locationDto.getStreetName().trim().equals("")
